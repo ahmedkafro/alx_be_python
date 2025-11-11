61acd909786ed4c999e485880230ffa1d4dcf0a8
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2025-11-11 10:25:10 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-
+number1 = int(10)
+number2 = int(5)
+sum = number1 + number2
+sub = number1 - number2
+Mul = number1 * number2
+print ( "Addition of 10 and 5 is" , sum )
+print ( "Subtraction of 10 and 5 is"  , sub )
+print ("Multiplication of 10 and 5 is"  ,Mul )
