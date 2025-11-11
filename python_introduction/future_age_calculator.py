d7314ecc7e5fb692a08aca5540732ce05dd4472b
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2025-11-11 15:45:50 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,1 +1,21 @@
+def user_future_age_calculator(user_input :int ,the_current_year :int , the_future_year:int )->int :
+        user_input = 27
+       
+   return (the_future_year - the_current_year ) + user_input
 
+def main():
+      the_future_year  = 2050
+      the_current_year = 2023
+      user_age  = (the_future_year - the_current_year ) + user_input
+  
+      print ("In " , the_future_year , "you will be" , user_age ,  "years old" )
+
+
+if __name__ == "__main__":
+    main()
+  
+
+
+    
+
+ 
