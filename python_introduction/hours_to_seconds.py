4c1348cba7f858eeff761a58c1f89ea111ae18c9
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2025-11-11 14:31:38 +0200

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,1 +1,16 @@
+def convert_hours_to_seconds(hours : int )->int:
 
+     SECONDS_PER_HOUR = 3600
+     return hours * SECONDS_PER_HOUR
+
+def main():
+
+    hours = 2  # Number of hours to convert
+    seconds = convert_hours_to_seconds(hours)
+    print(f"{hours} hour(s) is {seconds} seconds.")
+
+
+if __name__ == "__main__":
+    main()
+ 
+  
