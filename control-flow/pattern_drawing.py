36b033ec8d536c0466e20a30c73dd21e400111f9
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2025-11-17 13:53:51 +0200

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,13 @@
 
+user_prompt = int(input("Enter the size of the pattern: "))
 
+n = 0
+
+while n < user_prompt:
+    for i in range(user_prompt):
+        print("*", end="")
+    print()   # move to next line
+    n += 1
 
 
 
