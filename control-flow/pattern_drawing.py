d5a9a3a9891fc55cd0175171656f304c5f3ec2ab
ahmedkafro <167016211+ahmedkafro@users.noreply.github.com>
2025-11-17 13:48:48 +0200

    Remove pattern drawing functionality
    Removed the pattern drawing logic that prints asterisks based on user input.

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,12 +1,4 @@
-user_prompt = int(input("Enter the size of the pattern: "))
 
-n = 0
-
-while n < user_prompt:
-    for i in range(user_prompt):
-        print("*", end="")
-    print()   # move to next line
-    n += 1
 
 
 
